baaa4eda3c3454f498506801e16aca5180e0e95d
Mary <l.skyling@gmail.com>
2017-09-12 14:20:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,12 +7,12 @@ function addElementToEndOfArray(A, b) {
 
 
 function addElementToBeginningOfArray(array, element){
-  array.unshift(element);
-  return array;
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element){
   var newArray = [...array];
   newArray.unshift(element);
   return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
 }
